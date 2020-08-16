7fe5d54c2391c030d6d32dd5e2357e929ee97ef9
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-16 15:53:18 -0400

    Starting Creating Form

--- README.md
+++
@@ -1,1 +0,0 @@
-# QuizCreator_frontend
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-    //logic to hide div 
-
     const main = document.querySelector("#main");
+    const createQuizButton = document.querySelector(".btn-create-quiz");
+    const joinClassButton = document.querySelector(".btn-join-classrom");
+    const hostClassButton = document.querySelector(".btn-host-classrom");
 
-/*     document.addEventListener("click", e => {
-        console.log(e.target)
-    }) */
+    const clickHandler = () => {
+        document.addEventListener("click", e => {
+            if (e.target.matches(".btn-create-quiz")){
+                const createContainer = document.querySelector("#create-container")
+                createContainer.style.display = "block"
+                const quizForm = document.createElement("form")
+                console.log(quizForm)
+            } else if (e.target.matches(".btn-join-classrom")) {
+
+            } else if (e.target.matches(".btn-host-classrom")) {
+
+            }
+        })
+    }
+    clickHandler();
+
+
+
 
 })
